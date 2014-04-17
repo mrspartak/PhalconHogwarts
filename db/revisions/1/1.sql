4c79281884b63ae5651386601ee0606880199865
Spartak Home <spartak@digicast.ru>
2014-04-17 13:22:07 +0400

    revisions

---
+++ db/revisions/1/1.sql
@@ -0,0 +1,5 @@
+INSERT INTO `faculty` (`id`, `name`, `logo`) VALUES
+(1, 'Грифиндор', 'img/faculty/g.png'),
+(2, 'Когтевран', 'img/faculty/r.png'),
+(3, 'Слизерин', 'img/faculty/s.png'),
+(4, 'Пуфендуй', 'img/faculty/h.png');
